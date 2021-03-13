9e63a7d8ae00e8e47b1f4594723ab0a9b8abe397
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-13 08:23:12 -0800

    Calculator

---
+++ js/js.js
@@ -0,0 +1,37 @@
+function insert(num)
+	{
+		document.form.textview.value=document.form.textview.value+num;
+	}
+	function equal()
+	{
+		var exp=document.form.textview.value;
+		if (exp) 
+		{
+			document.form.textview.value=eval(exp);
+		}
+	}
+	function clear()
+	{
+		document.form.textview.value="";
+	}
+	function back()
+	{
+		var exp=document.form.textview.value;
+		document.form.textview.value=exp.substring(0,exp.length-1);
+	}
+	function div()
+	{
+
+	}
+	function mul()
+	{
+
+	}
+	function add()
+	{
+
+	}
+     function sub()
+     {
+     	
+     }
